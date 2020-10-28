d65a16a875dcd8ad198816e1621a8ed5921a1354
roger <a1063333@mail.nuk.edu.tw>
2020-10-28 09:46:07 +0800

    add project

---
+++ dice_flutter/android/app/src/main/kotlin/com/roger/dice_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.roger.dice_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ quiz_flutter/android/app/src/main/kotlin/com/roger/quiz_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.roger.quiz_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
